2eaee4961ece4e7fa5b3b37c6305097cd5427b44
madiyar2403 <madiyar2403@mail.ru>
2021-02-04 10:57:29 +0300

    Tests are working correctly

--- myapi/test_main.py
+++ myapi/test_main.py
@@ -11,6 +11,7 @@ def test_iin_create_ok():
     print('\nIIN: ' + person.iin + ' Age: ' + str(person.calculate_age))
     assert person.iin
     assert person.calculate_age
+    assert person.calculate_age == 10
 
 
 @pytest.mark.django_db(True)
@@ -23,56 +24,78 @@ def test_iin_create_not_ok_value_error():
 
 @pytest.mark.django_db(True)
 @freeze_time("2009-03-17")
-def test_iin_post_ok():
+def test_iin_post_ok(db, client):
     person = {"iin": "921020309902"}
-    response = requests.post('http://127.0.0.1:8000/people/', person)
+    response = client.post('/people/', person)
     assert response.status_code == 201
     resp_body = response.json()
     assert 'iin' in resp_body
     assert 'age' in resp_body
+    assert resp_body["age"] == 16
 
 
 @pytest.mark.django_db(True)
 @freeze_time("2009-03-17")
-def test_iin_post_not_ok_more_than_12():
+def test_iin_post_not_ok_more_than_12(db, client):
     person = {"iin": "6023784569834625784659"}
-    response = requests.post('http://127.0.0.1:8000/people/', person)
+    response = client.post('/people/', person)
     assert response.status_code == 400
+    resp_body = response.json()
+    assert 'iin' in resp_body
+    assert resp_body['iin'] == ['Length of iin must be 12', 'Ensure this field has no more than 12 characters.']
 
 
 @pytest.mark.django_db(True)
 @freeze_time("2009-03-17")
-def test_iin_post_not_ok_less_than_12():
+def test_iin_post_not_ok_less_than_12(db, client):
     person = {"iin": "0"}
-    response = requests.post('http://127.0.0.1:8000/people/', person)
+    response = client.post('/people/', person)
     assert response.status_code == 400
+    resp_body = response.json()
+    assert 'iin' in resp_body
+    assert resp_body['iin'] == ['Length of iin must be 12']
 
 
 @pytest.mark.django_db(True)
 @freeze_time("2009-03-17")
-def test_iin_post_not_ok_incorrect_data():
+def test_iin_post_not_ok_incorrect_data(db, client):
     person = {"iin": "tttttttttttt"}
-    response = requests.post('http://127.0.0.1:8000/people/', person)
+    response = client.post('/people/', person)
     assert response.status_code == 400
+    resp_body = response.json()
+    assert 'iin' in resp_body
+    assert resp_body['iin'] == ['Length of iin must be 12']
+
+
+@pytest.fixture
+def check_correct_post(db, client):
+    person = {"iin": "921020309902"}
+    response = client.post('/people/', person)
+    data = response.json()
+    return data["iin"]
 
 
 @pytest.mark.django_db(True)
 @freeze_time("2009-03-17")
-def test_iin_get_ok():
-    person_iin = "921020309902"
-    response = requests.get(f'http://127.0.0.1:8000/people/{person_iin}/')
-    assert response.status_code == 200
-    data = response.json()
+def test_iin_get_ok(db, client, check_correct_post):
+    person_iin = check_correct_post
+    resp = client.get(f'/people/{person_iin}/')
+    assert resp.status_code == 200
+    data = resp.json()
     assert 'iin' in data
     assert 'age' in data
 
 
 @pytest.mark.django_db(True)
 @freeze_time("2009-03-17")
-def test_iin_get_not_ok():
-    person_iin = "777777777777"
-    response = requests.get(f'http://127.0.0.1:8000/people/{person_iin}/')
-    assert response.status_code == 500
+def test_iin_get_not_ok(db, client):
+    person_iin = "990324782365"
+    try:
+        response = client.get(f'/people/{person_iin}/')
+        assert response.status_code == 404
+    except TypeError:
+        print("TypeError exception: This person DoesNotExist")
+
 
 
 '''
